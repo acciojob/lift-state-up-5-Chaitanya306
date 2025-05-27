f2302e8dae2e88f2b855f1c83d9d9af5effbf32e
Chaitanya306 <chaitanya09306@gmail.com>
2025-05-27 08:54:29 +0530

    changes-1

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Form(props) {
 
-  const {changeLoginfo}=props
+  const {changeLoginfo,isLoggedIn}=props
   function formSubmit(event){
     event.preventDefault()
     //console.log('logged in')
@@ -10,15 +10,18 @@ export default function Form(props) {
   }  
   return (
     <div>
-      <form onSubmit={formSubmit}>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required />
+      {isLoggedIn ? <p>You are logged in!</p>:
+        <form onSubmit={formSubmit}>
+        <label htmlFor="username">Username:</label><br></br>
+        <input type="text" id="username" name="username" required /><br></br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required />
+        <label htmlFor="password">Password:</label><br></br>
+        <input type="password" id="password" name="password" required /><br></br>
 
         <button type="submit">Login</button>
-      </form>
+        </form>
+      }
+      
     </div>
   )
 }
